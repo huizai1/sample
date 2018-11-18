a0196792cd347beb1fd2632dfb8d697657c361b0
huizai <237002893@qq.com>
2018-11-18 06:36:46 +0000

    Finish static pages

---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.default')
+@section('title','帮助')
+
+@section('content')
+    <h1>帮助页修改</h1>
+@stop
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.default')
+@section('content')
+    <h1>主页修改</h1>
+@stop
